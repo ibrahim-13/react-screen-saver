39048960db2b74530bad99cd8e06cae79abe89d8
ibrahim-13 <ib.arshad777@gmail.com>
2020-03-14 10:57:08 +0600

    Integrated remote images into embedded images

--- src/animated-bg/index.tsx
+++ src/animated-bg/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './bg.css';
+import WaveTop from './wave-top.png';
+import WaveMid from './wave-mid.png';
+import WaveBot from './wave-bot.png';
 
 const AnimatedBackground: React.FC = () => {
+  console.log()
   return (
     <div className="waveWrapper waveAnimation">
       <div className="waveWrapperInner bgTop">
-        <div className="wave waveTop" style={{ backgroundImage: "url('http://front-end-noobs.com/jecko/img/wave-top.png')" }}></div>
+        <div className="wave waveTop" style={{ backgroundImage: `url('${WaveTop}')` }}></div>
       </div>
       <div className="waveWrapperInner bgMiddle">
-        <div className="wave waveMiddle" style={{ backgroundImage: "url('http://front-end-noobs.com/jecko/img/wave-mid.png')" }}></div>
+        <div className="wave waveMiddle" style={{ backgroundImage: `url('${WaveMid}')` }}></div>
       </div>
       <div className="waveWrapperInner bgBottom">
-        <div className="wave waveBottom" style={{ backgroundImage: "url('http://front-end-noobs.com/jecko/img/wave-bot.png')" }}></div>
+        <div className="wave waveBottom" style={{ backgroundImage: `url('${WaveBot}')` }}></div>
       </div>
     </div>
   );
